c6a39fd0fe8f12002da583f8d71db8304e9e3124
Bủ Bủ <chaungocbuudang2003@gmail.com>
2023-11-01 15:01:13 +0700

    update border

--- my-app/src/components/Button.jsx
+++ my-app/src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = (props) => {
   return (
     <div>
-      <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
         {props.title}
       </button>
     </div>
--- my-app/src/components/DoctorCard.jsx
+++ my-app/src/components/DoctorCard.jsx
@@ -1,36 +1,8 @@
-// import React from 'react'
-// import { doctors } from '../constants';
-// const DoctorCard = (id) => {
-//   // {imgURL, name, faculty, info, hospital }
-//   return (
-//     <div class="max-w-sm rounded overflow-hidden shadow-lg">
-//       <img src={doctors[id].imgURL} alt="Doc1" className="w-full" />
-//       <div class="px-6 py-4">
-//         <div class="font-bold text-xl mb-2">{doctors[id].name}</div>
-//         <p class="text-gray-700 text-base">
-//         {doctors[id].info}
-//         </p>
-//       </div>
-//       <div class="px-6 pt-4 pb-2">
-//         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-//         {doctors[id].hospital}
-//         </span>
-//         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-//         {doctors[id].faculty}
-//         </span>
-
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default DoctorCard
-
 import React from "react";
 import Button from "./Button";
 const DoctorCard = ({ imgURL, name, faculty, info, hospital }) => {
   return (
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <div class="max-w-sm rounded overflow-hidden hover:shadow-md border-2 border-blue-50">
       <img src={imgURL} alt="Doc1" className="w-full" />
       <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">{name}</div>
