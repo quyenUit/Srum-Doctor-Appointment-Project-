2e4fdd8da30858bdbc73574b871a95bd3beb584a
Bủ Bủ <chaungocbuudang2003@gmail.com>
2023-11-01 13:35:50 +0700

    update homepage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-
+import HomePage from './pages/HomePage'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.app}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
     </div>
   );
+
 }
+const styles = {
+  app: {
+    padding: 50,
+  },
+};
 
 export default App;
